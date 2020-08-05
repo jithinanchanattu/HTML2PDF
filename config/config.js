d3a440e8274c3bf2d08898d1839c807bf36468a4
Jithin Joji Anchanattu <jithin.anchanattu@gmail.com>
2020-08-05 17:23:55 +0530

    stable fix

---
+++ config/config.js
@@ -0,0 +1,32 @@
+//satic data to don't have to generate the conf_adata 2 times
+let config_data = null
+module.exports = function() {
+// if the static data was already set. return it
+if(config_data != null && config_data != undefined) {
+        return config_data
+}
+
+config_data = {
+    "port": 8080,
+    "static_variable": "static",
+    "randFileName": false,
+    "pdf": {
+        "defFileName":"exportPDF",
+        "defPath": "./dump/",
+        "format": "A4",
+        "displayHeaderFooter": true,
+        "headerTemplate": '<div style="text-align: center;background: #1abc9c;color: white;font-size: 30px;"><h1>Header</h1><p>My supercool header</p></div>',
+        "printBackground": true,
+        "margin": {
+            "left": "0px",
+            "top": "200px",
+            "right": "0px",
+            "bottom": "0px",
+        }
+    },
+    "mergePDF":{
+        "destPath":"./dump"
+    }
+}
+return config_data
+}
--- http.js
+++ http.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const url = require('url');
 const puppeteer = require('puppeteer');
+const combinePdfs = require("combine-multiple-pdfs");
+const config_data = require("./config/config.js")();
 
-async function printPDF(URLPath, PDFLink, PDFRand) {
+async function printPDF(URLPath, PDFLink, PDFRand, PDFMerge) {
     const browser = await puppeteer.launch({ headless: true, defaultViewport: null });
     const page = await browser.newPage();
     //waitUntil: ‘networkidle0’ option means that Puppeteer considers navigation to be finished
-    await page.goto(URLPath, {waitUntil: 'networkidle0'}); 
-    await page.waitFor('.home-template');
-    var rand             = '';
-    if(PDFRand == "Y" || PDFLink == undefined){
+    await page.goto(URLPath, {waitUntil: 'networkidle0', timeout: 0}); 
+    //await page.waitFor('.home-template');
+    var rand = '';
+    if((PDFRand == "Y" || PDFLink == undefined) && config_data.randFileName){
         //Random PDF Name    
         var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
@@ -20,36 +22,65 @@ async function printPDF(URLPath, PDFLink, PDFRand) {
     }else{
         PDFLink = PDFLink;
     }
+
+    if(PDFLink == undefined || PDFLink == null){
+      PDFLink = config_data.pdf.defFileName;
+    }
     
     const pdf = await page.pdf({ 
           path: PDFLink + '.pdf',
-          format: 'A4',
-          printBackground: true,
+          displayHeaderFooter: config_data.pdf.displayHeaderFooter,
+          headerTemplate: config_data.pdf.headerTemplate,
+          format: config_data.pdf.format,
+          printBackground: config_data.pdf.printBackground,
           margin: {
-              left: '0px',
-              top: '0px',
-              right: '0px',
-              bottom: '0px'
-          } 
+              left: config_data.pdf.margin.left,
+              top: config_data.pdf.margin.top,
+              right: config_data.pdf.margin.right,
+              bottom: config_data.pdf.margin.bottom
+          }
       });
     await browser.close();
     //return pdf; //This will return a buffer with the proper content type to the callback
   };
 
+async function mergePDF(pdfPath, PDFMerge) {
+  var dest_file_path = config_data.mergePDF.destPath;
+  let data = pdfPath;
+  if(PDFMerge == "Y"){
+    let options = { outName: "mergedPdf", outPath: dest_file_path };
+    let newFileBuffer = combinePdfs(data, options);
+    console.log('Merged to PDF');
+  }
+}
 http.createServer(function (req, res) {
   const queryObject = url.parse(req.url,true).query;
-  var URLPath, PDFLink, PDFRand;
-  for (var p in queryObject){    
-      if(queryObject[p] != undefined && p == "URLPath"){
-        URLPath = Buffer.from(queryObject[p], 'base64').toString();
-        console.log(URLPath);
-        var buffer = printPDF(URLPath, queryObject[PDFLink], queryObject[PDFRand]).then((response) => {
-            console.log('Converting to PDF');
-          })
-        .catch(err => console.log(err));
-      }    
-  } 
+  var URLPath, PDFLink, PDFRand, PDFMerge;
+  
+  for (var p2 in queryObject){    
+    if(queryObject[p2] != undefined && p2 == "PDFLink"){
+      PDFLink = queryObject[p2];
+    }
+  }
+  for (var p3 in queryObject){    
+    if(queryObject[p3] != undefined && p3 == "PDFMerge"){
+      PDFMerge = queryObject[p3];
+    }
+  }
+
+  for (var p in queryObject){  
+    if(queryObject[p] != undefined && p == "URLPath"){
+      URLPath = Buffer.from(queryObject[p], 'base64').toString();
+
+      var buffer = printPDF(URLPath, PDFLink, "", PDFMerge).then((response) => {
+        console.log('Converted to PDF');
+        mergePDF(["./1.pdf", "./2.pdf", "./3.pdf"], "Y");
+      })
+    .catch(err => console.log(err));
+    }
+  }
+
 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Feel free to add query parameters to the end of the url. <br/>Example: http://localhost:8080/http.js?URLPath=aHR0cHM6Ly9ibG9nLnJpc2luZ3N0YWNrLmNvbQ==&PDFRand=Y&PDFLink=RzpcSUJTXFBERkV4cG9ydFxFeHBvcnRDb21tYW5kZXJc');
+  res.end("Feel free to add query parameters to the end of the url. <br/>Sample Request Links: <br/>http://localhost:8080/http.js?<b>URLPath</b>=aHR0cHM6Ly9ibG9nLnJpc2luZ3N0YWNrLmNvbQ==&<b>PDFRand</b>=N&<b>PDFMerge</b>=Y&<b>PDFLink</b>=ExportPDF <br/> http://localhost:8080/http.js?<b>URLPath</b>=aHR0cHM6Ly9ibG9nLnJpc2luZ3N0YWNrLmNvbQ==&<b>PDFRand</b>=Y&<b>PDFMerge</b>=Y&<b>PDFLink</b>=G:\\IBS\\PDFExport\\ExportCommander\\<br/><br/>  <p>Enter the URL below details to create link: </p><input value=\"https://blog.risingstack.com/\" placeholder=\"genURL\" id=\"genURL\" onchange=\"document.getElementById('URLPath').innerHTML = btoa(document.getElementById('genURL').value)\"></input><input placeholder=\"Merge PDF (Y/N)\" id=\"mergePDF\" onchange=\"document.getElementById('PDFMerge').innerHTML = document.getElementById('mergePDF').value\"></input><input placeholder=\"genPDF FileName\" id=\"genFileName\" onchange=\"document.getElementById('PDFLink').innerHTML = (document.getElementById('genFileName').value)\"></input><p>http://localhost:8080/http.js?URLPath=<span id=\"URLPath\"></span>&amp;PDFRand=N&amp;PDFMerge=<span id=\"PDFMerge\"></span>&amp;PDFLink=<span id=\"PDFLink\"></span></p>");
 }).listen(8080);
